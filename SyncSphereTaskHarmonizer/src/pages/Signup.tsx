9c10dce1e69e3f55bfe81e22b1d89a60196145a2
Andrian Barbulat <andrian.barbulat@gmail.com>
2024-01-21 23:49:23 +0000

    adding signup feature

---
+++ SyncSphereTaskHarmonizer/src/pages/Signup.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
+const Signup: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Sign Up</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Signup;
