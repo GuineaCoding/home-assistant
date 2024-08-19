ebd2db6122fe874672a3038a20424dd4a8c0fbad
Andrian Barbulat <andrian.barbulat@gmail.com>
2024-08-19 23:47:38 +0100

    sign-in page

---
+++ SyncSphereTaskHarmonizer/src/pages/sign-in.tsx
@@ -0,0 +1,19 @@
+<ion-header>
+  <ion-toolbar>
+    <ion-title>Sign In</ion-title>
+  </ion-toolbar>
+</ion-header>
+
+<ion-content class="ion-padding">
+  <form>
+    <ion-item>
+      <ion-label position="floating">Username</ion-label>
+      <ion-input type="text" required></ion-input>
+    </ion-item>
+    <ion-item>
+      <ion-label position="floating">Password</ion-label>
+      <ion-input type="password" required></ion-input>
+    </ion-item>
+    <ion-button expand="block" type="submit" class="ion-margin-top">Sign In</ion-button>
+  </form>
+</ion-content>
