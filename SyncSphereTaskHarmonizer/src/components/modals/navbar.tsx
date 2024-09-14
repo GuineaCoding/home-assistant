67c37d5e60fd91d00e43fc09581505bf55aa7fd8
Andrian Barbulat <andrian.barbulat@gmail.com>
2024-09-14 23:37:23 +0100

    logout integration frontend

--- SyncSphereTaskHarmonizer/src/components/modals/navbar.tsx
+++ SyncSphereTaskHarmonizer/src/components/modals/navbar.tsx
@@ -1,30 +1,38 @@
+import { logOutOutline } from 'ionicons/icons';
 import {
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonButtons,
-    IonMenuButton,
-    IonContent,
-    IonPage
-  } from '@ionic/react';
-  
-  const NavBar: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot="start">
-              <IonMenuButton />
-            </IonButtons>
-            <IonTitle>My App</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-     
-          <p>This is the page content!</p>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default NavBar;
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonButtons,
+  IonMenuButton,
+  IonContent,
+  IonPage,
+  IonButton,
+  IonIcon
+} from '@ionic/react';
+
+const NavBar: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>My App</IonTitle>
+          <IonButtons slot="end">
+            <IonButton>
+              <IonIcon icon={logOutOutline} />
+              Logout
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <p>This is the page content!</p>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default NavBar;
