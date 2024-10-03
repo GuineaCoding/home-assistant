ea38139f0b1ca3fe96453e2836723759c30a2842
Andrian Barbulat <andrian.barbulat@gmail.com>
2024-10-03 23:41:11 +0100

    delete note functionality

--- SyncSphereTaskHarmonizer/src/pages/Notes.tsx
+++ SyncSphereTaskHarmonizer/src/pages/Notes.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItemGroup, IonItemDivider, IonLabel, IonItem, IonFab, IonFabButton, IonIcon, IonButton, } from '@ionic/react';
-import { add } from 'ionicons/icons';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonLabel, IonItem, IonFab, IonFabButton, IonIcon, IonButton } from '@ionic/react';
+import { add, trash } from 'ionicons/icons';
 import { goToAddNotes } from '../components/general-functionality/redirect/RedirectToPages';
 import { getAuth } from 'firebase/auth';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, doc, deleteDoc } from 'firebase/firestore';
 import { db } from '../environments/environment';  
-import { trash } from 'ionicons/icons';
+
 const Notes: React.FC = () => {
   const [notes, setNotes] = useState<any[]>([]); 
   const [loading, setLoading] = useState(true);  
@@ -16,23 +16,33 @@ const Notes: React.FC = () => {
     const fetchNotes = async () => {
       const user = auth.currentUser;
       if (user) {
-       
         const userNotesCollection = collection(db, `notes/${user.uid}/userNotes`);
-        
-
         const notesSnapshot = await getDocs(userNotesCollection);
-        
         const userNotes = notesSnapshot.docs.map(doc => ({
           id: doc.id,
           ...doc.data(),
         }));
-        
         setNotes(userNotes);
         setLoading(false);
       }
     };
     fetchNotes();
   }, [auth]);
+
+  const deleteNote = async (noteId: string) => {
+    try {
+      const user = auth.currentUser;
+      if (user) {
+        const noteDocRef = doc(db, `notes/${user.uid}/userNotes/${noteId}`);
+        await deleteDoc(noteDocRef);
+
+        // Update the UI by filtering out the deleted note
+        setNotes(prevNotes => prevNotes.filter(note => note.id !== noteId));
+      }
+    } catch (error) {
+      console.error("Error deleting note: ", error);
+    }
+  };
 
   if (loading) {
     return <IonContent>Loading notes...</IonContent>;
@@ -49,9 +59,13 @@ const Notes: React.FC = () => {
         <IonList>
           {notes.map((note, index) => (
             <IonItem key={note.id || index}>
-              <IonLabel>{note.title}</IonLabel>
-              <IonLabel>{note.content}</IonLabel>
-              <IonIcon icon={trash} aria-label="Favorite" />
+              <IonLabel>
+                <h2>{note.title}</h2>
+                <p>{note.content}</p>
+              </IonLabel>
+              <IonButton color="danger" fill="clear" onClick={() => deleteNote(note.id)}>
+                <IonIcon icon={trash} />
+              </IonButton>
             </IonItem>
           ))}
         </IonList>
