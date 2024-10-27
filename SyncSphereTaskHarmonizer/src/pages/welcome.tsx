7b7e7351244a4d70b6daae621104fb63a8ddd6b4
Andrian Barbulat <andrian.barbulat@gmail.com>
2024-10-27 23:37:58 +0000

    Update welcome page

--- SyncSphereTaskHarmonizer/src/pages/welcome.tsx
+++ SyncSphereTaskHarmonizer/src/pages/welcome.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { IonContent, IonButton, IonPage } from '@ionic/react';
+import { IonContent, IonButton, IonPage, IonText, IonGrid, IonRow, IonCol, IonImg } from '@ionic/react';
 import SignInModal from '../components/modals/SignInModal';
 import SignUpModal from '../components/modals/SignUpModal';
 
@@ -9,12 +9,34 @@ const WelcomePage: React.FC = () => {
 
   return (
     <IonPage>
-      <IonContent className="ion-padding">
-        <IonButton expand="block" onClick={() => setShowSignIn(true)}>Sign In</IonButton>
-        <SignInModal show={showSignIn} onClose={() => setShowSignIn(false)} />
-
-        <IonButton expand="block" onClick={() => setShowSignUp(true)}>Sign Up</IonButton>
-        <SignUpModal show={showSignUp} onClose={() => setShowSignUp(false)} />
+      <IonContent className="ion-padding ion-text-center">
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonText color="primary" style={{ fontSize: '1.5em' }}>
+                Welcome to Our App!
+              </IonText>
+              <IonText>
+                Get started by signing in or creating a new account.
+              </IonText>
+            </IonCol>
+          </IonRow>
+          <IonRow style={{ marginTop: '20px' }}>
+            <IonCol>
+              <IonButton expand="block" onClick={() => setShowSignIn(true)}>Sign In</IonButton>
+              <SignInModal show={showSignIn} onClose={() => setShowSignIn(false)} />
+            </IonCol>
+            <IonCol>
+              <IonButton expand="block" onClick={() => setShowSignUp(true)}>Sign Up</IonButton>
+              <SignUpModal show={showSignUp} onClose={() => setShowSignUp(false)} />
+            </IonCol>
+          </IonRow>
+          <IonRow style={{ marginTop: '40px' }}>
+            <IonCol>
+              <IonImg src="/assets/images/welcome_image.png" style={{ maxWidth: '100%', height: 'auto' }} />
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
