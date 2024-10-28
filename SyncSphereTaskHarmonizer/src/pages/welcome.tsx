dba642b69014960970f360b6c208fcb7358065ef
Andrian Barbulat <andrian.barbulat@gmail.com>
2024-10-28 00:14:31 +0000

    update view

--- SyncSphereTaskHarmonizer/src/pages/welcome.tsx
+++ SyncSphereTaskHarmonizer/src/pages/welcome.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { IonContent, IonButton, IonPage, IonText, IonGrid, IonRow, IonCol, IonImg } from '@ionic/react';
+import {
+  IonContent, IonButton, IonPage, IonText, IonGrid, IonRow, IonCol, IonImg, IonModal
+} from '@ionic/react';
 import SignInModal from '../components/modals/SignInModal';
 import SignUpModal from '../components/modals/SignUpModal';
 
@@ -9,31 +11,35 @@ const WelcomePage: React.FC = () => {
 
   return (
     <IonPage>
-      <IonContent className="ion-padding ion-text-center">
-        <IonGrid>
+      <IonContent fullscreen className="ion-padding ion-text-center">
+        <IonGrid fixed>
           <IonRow>
             <IonCol>
-              <IonText color="primary" style={{ fontSize: '1.5em' }}>
-                Welcome to Our App!
+              <IonText color="primary" style={{ fontSize: '1.8em', fontWeight: 'bold' }}>
+                Welcome to Our Amazing App!
               </IonText>
-              <IonText>
-                Get started by signing in or creating a new account.
-              </IonText>
+              <p style={{ fontSize: '1.2em', marginTop: '15px' }}>
+                Access all the features by signing in or signing up today.
+              </p>
             </IonCol>
           </IonRow>
-          <IonRow style={{ marginTop: '20px' }}>
-            <IonCol>
+          <IonRow className="ion-justify-content-center" style={{ marginTop: '30px' }}>
+            <IonCol size="12" size-md="6">
               <IonButton expand="block" onClick={() => setShowSignIn(true)}>Sign In</IonButton>
-              <SignInModal show={showSignIn} onClose={() => setShowSignIn(false)} />
+              <IonModal isOpen={showSignIn} onDidDismiss={() => setShowSignIn(false)} swipeToClose={true}>
+                <SignInModal onClose={() => setShowSignIn(false)} />
+              </IonModal>
             </IonCol>
-            <IonCol>
+            <IonCol size="12" size-md="6">
               <IonButton expand="block" onClick={() => setShowSignUp(true)}>Sign Up</IonButton>
-              <SignUpModal show={showSignUp} onClose={() => setShowSignUp(false)} />
+              <IonModal isOpen={showSignUp} onDidDismiss={() => setShowSignUp(false)} swipeToClose={true}>
+                <SignUpModal onClose={() => setShowSignUp(false)} />
+              </IonModal>
             </IonCol>
           </IonRow>
-          <IonRow style={{ marginTop: '40px' }}>
+          <IonRow>
             <IonCol>
-              <IonImg src="/assets/images/welcome_image.png" style={{ maxWidth: '100%', height: 'auto' }} />
+              <IonImg src="/assets/images/welcome_image.svg" style={{ maxWidth: '100%', height: 'auto', marginTop: '40px' }} />
             </IonCol>
           </IonRow>
         </IonGrid>
